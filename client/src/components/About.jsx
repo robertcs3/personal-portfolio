3ef28c4a85fea52452643f5cf473fe4bda18dcd3
Bobby <48532176+robertcs3@users.noreply.github.com>
2023-01-20 22:08:27 -0600

    About section styling

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -14,7 +14,89 @@ export default function About() {
       </section>
       
       <section className='flex justify-center items-center row-span-4'>
-        MY TECH STACK
+        <div className='grid grid-cols-4 grid-rows-4 gap-4'>
+
+          <div className='tech-set'>
+          <i className="devicon-html5-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>HTML</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-css3-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>CSS</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-javascript-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Javascript</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-typescript-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Typescript</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-react-original colored tech-icon"></i>
+          <span className='tech-icon-name'>React</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-express-original colored tech-icon"></i>
+          <span className='tech-icon-name'>Express</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-nodejs-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>NodeJs</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-mongodb-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>MongoDB</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-tailwindcss-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Tailwind</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-bootstrap-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Bootstrap</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-jest-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Jest</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-java-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Java</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-git-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Git</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-npm-original-wordmark colored tech-icon"></i>
+          <span className='tech-icon-name'>Npm</span>
+          </div>
+
+          <div className='tech-set'>
+          <i class="devicon-yarn-plain colored tech-icon"></i>
+          <span className='tech-icon-name'>Yarn</span>
+          </div>
+
+          <div className="tech-set">
+            <i class="devicon-visualstudio-plain colored tech-icon"></i>
+            <span className='tech-icon-name'>VS Code</span>
+            </div>
+
+        </div>
       </section>
     </div>
   )
