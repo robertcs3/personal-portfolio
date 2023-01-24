6304684bd7270329d9f31d85896837aac99c69b6
Bobby <48532176+robertcs3@users.noreply.github.com>
2023-01-23 23:59:01 -0600

    Contact section
    reponsiveness styling

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Formik } from 'formik'
 import * as yup from 'yup';
-import Axios from 'axios';
+
 
 const schema = yup.object().shape(({
   name: yup.string().required(),
@@ -11,67 +11,75 @@ const schema = yup.object().shape(({
 
 export default function Contact() {
 
-  
+
   return (
-    <div id='contact-page' className='flex flex-col items-center min-h-min container mx-auto text-xl mb-12'>
+    <div id='contact-page' className='flex flex-col items-center container mx-auto text-lg mb-12'>
       <header>Contact</header>
-    
-      <p className='mb-10'>If you have an opportunity to share, or would like to discuss anything, feel free to leave me a message below!</p>
+
+      <p className='mb-10 w-full sm:max-w-lg'>If you have an opportunity to share, or would like to discuss anything, feel free to leave me a message below!</p>
       <Formik
-      validationSchema={schema}
-      initialValues={{
-        name: '',
-        email: '',
-        message: '',
-      }}
+        validationSchema={schema}
+        initialValues={{
+          name: '',
+          email: '',
+          message: '',
+        }}
 
-      onSubmit = {(values, actions) => {
-        console.log(values.name, values.email, values.message)
-        const data = {
-          name: values.name,
-          email: values.email,
-          message: values.message,
-        }
+        onSubmit={(values, actions) => {
+          console.log(values.name, values.email, values.message)
+          const data = {
+            name: values.name,
+            email: values.email,
+            message: values.message,
+          }
 
-        const url = 'https://script.google.com/macros/s/AKfycbz6ZUMGOK2r2eQ3_gZ3ym6HkHfdeJ5mzKo9H_YqAW74imXIqxI-iMpE1Jrq9PFwS21Xqw/exec'
+          const url = 'https://script.google.com/macros/s/AKfycbz6ZUMGOK2r2eQ3_gZ3ym6HkHfdeJ5mzKo9H_YqAW74imXIqxI-iMpE1Jrq9PFwS21Xqw/exec'
 
-        fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'text/plain;charset=utf-8',
-          },
-          body: JSON.stringify(data),
-        }).then((res) => res.json())
-        .then((data) => console.log('data', data))
-        .catch((err) => console.log('err', err));
-      }}
+          fetch(url, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'text/plain;charset=utf-8',
+            },
+            body: JSON.stringify(data),
+          }).then((res) => res.json())
+            .then((data) => console.log('data', data))
+            .catch((err) => console.log('err', err));
+          actions.resetForm({
+            values: {
+              name: '',
+              email: '',
+              message: '',
+            }
+          })
+        }}
       >
         {({
           values,
-          errors,
-          touched,
           handleChange,
           handleBlur,
           handleSubmit,
-          isSubmitting,
         }) => (
 
-        <form onSubmit={handleSubmit} className='flex flex-col gap-3' >
-          <input className='bg-slate-800' type="text" name='name' placeholder='Name' onChange={handleChange} onBlur={handleBlur} value={values.name}  required/>
-         
-          <input className='bg-slate-800' type="email" placeholder='E-mail' name='email' onChange={handleChange} onBlur={handleBlur} value={values.email} required/>
-          
-          <textarea className='bg-slate-800' name="message" id="message" cols="30" rows="5" placeholder='Message'
-          onChange={handleChange} onBlur={handleBlur} value={values.message} required></textarea>
-         
-          <button type='submit'>Submit</button>
-        </form>
-    
+          <form onSubmit={handleSubmit} className='flex flex-col gap-3 w-full sm:max-w-lg' >
+            <input className='bg-slate-800 py-1 pl-1 ' type="text" name='name' placeholder='Name' onChange={handleChange} onBlur={handleBlur} value={values.name} required />
+
+            <input className='bg-slate-800 py-1 pl-1' type="email" placeholder='E-mail' name='email' onChange={handleChange} onBlur={handleBlur} value={values.email} required />
+
+            <textarea className='bg-slate-800  py-1 pl-1' name="message" id="message" cols="30" rows="5" placeholder='Message'
+              onChange={handleChange} onBlur={handleBlur} value={values.message} required></textarea>
+
+
+            <div className='flex justify-end mb-12 pt-3'>
+              <button id='submit' className='border' type='submit'>Submit</button>
+            </div>
+
+          </form>
+
         )}
 
       </Formik>
 
-      
+
     </div>
   )
 }
